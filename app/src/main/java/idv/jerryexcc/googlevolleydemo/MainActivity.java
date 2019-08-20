5ff601f331e4870528f2b76ac03cdc753b26fb60
Killua <harima79215121@gmail.com>
2019-08-20 22:46:23 +0800

    init project

---
+++ app/src/main/java/idv/jerryexcc/googlevolleydemo/MainActivity.java
@@ -0,0 +1,38 @@
+package idv.jerryexcc.googlevolleydemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    RequestQueue requestQueue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //init
+        requestQueue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest request = new StringRequest(Request.Method.GET, "https://www.google.com.tw", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Response Listener
+                Log.d("TAG", "All HTML: " + response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //Error Listener
+            }
+        });
+        requestQueue.add(request);
+    }
+}
